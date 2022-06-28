55098c6420dd1c11141efccf4398ca54937ec799
manideepreddy13 <manideepreddy130702@gmail.com>
2022-06-28 21:22:38 +0530

    Added Arrays Files

--- Arrays/pascaltriangle.cpp
+++ Arrays/pascaltriangle.cpp
@@ -3,22 +3,21 @@
 #include <algorithm>
 using namespace std;
 
-
 vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> vec;
-        int i,j;
-        for(i=0;i<numRows;i++){
-            for(j=0;j<=i;j++){
-                if(j==0 || j == i){
-                    vec[i][j] = 1;
-                } else {
-                    vec[i][j] = vec[i-1][j-1] + vec[i-1][j];
-                }
+    vector<vector<int>> vec;
+    int i,j;
+    for(i=0;i<numRows;i++){
+        vector<int> temp;
+        for(j=0;j<=i;j++){
+            if(j==0 || j == i){
+                temp.push_back(1);
+            } else {
+                temp.push_back(vec[i-1][j-1] + vec[i-1][j]);
             }
         }
-        
-
-       return vec;
+        vec.push_back(temp);
+    }
+    return vec;
 }
 
 
