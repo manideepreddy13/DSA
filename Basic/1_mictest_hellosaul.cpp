0851fde9097fa6c1688d7570df3a576720742fcd
manideepreddy13 <manideepreddy130702@gmail.com>
2023-03-20 10:34:46 +0530

    Added Basic

---
+++ Basic/1_mictest_hellosaul.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+
+//General Skeleton of the C++ Program
+int main(){
+    cout << "Hola Mundo!!" << endl << "Estoy Programador";
+return 0;
+}
+
+//Note : We used endl instead of \n for newline
---
+++ Basic/3_conditional_statements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+
+//check whether the number is greater than 18 or not
+int main(){
+    int num;
+    cin>>num;
+
+    if(num>=18){
+        cout<<"Yes";
+    } else {
+        cout<<"No";
+    }
+return 0;
+}
